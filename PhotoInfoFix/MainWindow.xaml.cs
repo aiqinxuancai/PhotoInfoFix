74c3416441e5088745ab63196561519cbb436ec3
爱琴炫彩 <aiqinxuancai@163.com>
2020-08-31 15:22:43 +0800

    update to .Net Core 3.1

--- PhotoInfoFix/MainWindow.xaml.cs
+++ PhotoInfoFix/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing.Imaging;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -16,8 +15,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ExifLib;
-
+using System.Drawing.Imaging;
+using Ookii.Dialogs.Wpf;
+using PhotoInfoFix.Services;
 
 namespace PhotoInfoFix
 {
@@ -29,82 +29,47 @@ namespace PhotoInfoFix
         public MainWindow()
         {
             InitializeComponent();
-            EditPhoto(@"D:\pic\20170326-JP-精简", "2017:03:20 10:25:00", "2014:10:02 10:58:54");
+            //FixExifDateTime(@"D:\pic\20170326-JP-精简", "2017:03:20 10:25:00", "2014:10:02 10:58:54");
+
+
+
         }
 
-        private void EditPhoto(string photoPath, string photoTrueTime, string photoFalseTime)
+        private void selectButton_Click(object sender, RoutedEventArgs e)
         {
-            var files = Directory.GetFiles(photoPath); //获取所有照片路径
-
-            IFormatProvider culture = new CultureInfo("zh-CN", true);
-
-            DateTime timeTrue = DateTime.ParseExact(photoTrueTime, "yyyy:MM:dd HH:mm:ss", culture); //某个照片的实际拍摄时间
-            DateTime timeFalse = DateTime.ParseExact(photoFalseTime, "yyyy:MM:dd HH:mm:ss", culture); //该照片EXIF的记录时间
-
-            TimeSpan timeSpan = timeTrue - timeFalse; //计算照片时间差
-
-            foreach (var file in files)
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.Description = "Please select a folder.";
+            dialog.UseDescriptionForTitle = true; // This applies to the Vista style dialog only, not the old dialog.
+            if (!VistaFolderBrowserDialog.IsVistaFolderDialogSupported)
             {
-                //替代System.Drawing.Image.FromFile(file);
-                System.Drawing.Image image = GetImage(file);
+                MessageBox.Show(this, "Because you are not using Windows Vista or later, the regular folder browser dialog will be used. Please use Windows Vista to see the new dialog.", "Sample folder browser dialog");
+            }
+            if ((bool)dialog.ShowDialog(this))
+            {
+                pathTextBox.Text = dialog.SelectedPath;
+                UpdateFirstFileTime();
+            }
+        }
 
-                //获取Exif的DateTimeOriginal属性（36867）
-                PropertyItem pi = image.GetPropertyItem((int)ExifTags.DateTimeOriginal); 
+        private void UpdateFirstFileTime()
+        {
+            if (!string.IsNullOrWhiteSpace(pathTextBox.Text))
+            {
+                var files = Directory.GetFiles(pathTextBox.Text, "*.*", SearchOption.AllDirectories);
 
-                //转为时间文本
-                string oldTime = Encoding.ASCII.GetString(pi.Value);
-                oldTime = oldTime.Replace("\0", "");
-
-                Debug.WriteLine(file + " " + oldTime);
-                //时间文本格式化为DateTime
-                DateTime time = DateTime.ParseExact(oldTime, "yyyy:MM:dd HH:mm:ss", culture);
-
-                //由于是接着ExifToolGui没有改完的目录，所以只转换EXIF记录为timeFalse年份的，
-                //跨年的话要另外处理，因为我这里不跨年，所以简单点
-                if (time.Year == timeFalse.Year) 
+                foreach(string item in files)
                 {
-                    //得到正确的时间
-                    DateTime newTime = time + timeSpan;
-
-                    //转换为EXIF存储的时间格式
-                    string newTimeString = newTime.ToString("yyyy:MM:dd HH:mm:ss"); 
-                    pi.Value = Encoding.ASCII.GetBytes(newTimeString + "\0");
-
-                    //修改DateTimeOriginal属性和其他的时间属性
-                    image.SetPropertyItem(pi);
-                    pi.Id = (int)ExifTags.DateTime; //306
-                    image.SetPropertyItem(pi);
-                    pi.Id = (int)ExifTags.DateTimeDigitized; //36868
-                    image.SetPropertyItem(pi);
-
-                    //存回文件
-                    image.Save(file);
+                    System.Drawing.Image image = ExifFix.GetImage(item);
+                    if (image != null)
+                    {
+                        DateTime time = ExifFix.GetDateTimeOriginal(image);
+                        dateTimePicker.SelectedDateTime = time;
+                        break;
+                    }
                 }
 
-                image.Dispose();
             }
         }
 
-        private System.Drawing.Image GetImage(string filePath)
-        {
-            if (!File.Exists(filePath))
-            {
-                return null;
-            }
-
-            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-            {
-                byte[] bytes = new byte[fileStream.Length];
-                fileStream.Read(bytes, 0, bytes.Length);
-
-                MemoryStream memoryStream = new MemoryStream(bytes);
-                if (memoryStream != null)
-                {
-                    return System.Drawing.Image.FromStream(memoryStream);
-                }
-            }
-
-            return null;
-        }
     }
 }
---
+++ PhotoInfoFix/Services/ExifFix.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Drawing.Imaging;
+
+namespace PhotoInfoFix.Services
+{
+    public class ExifFix
+    {
+        public static void FixExifDateTime(string photoPath, string photoTrueTime, string photoFalseTime)
+        {
+            IFormatProvider culture = new CultureInfo("zh-CN", true);
+            DateTime timeTrue = DateTime.ParseExact(photoTrueTime, "yyyy:MM:dd HH:mm:ss", culture); //某个照片的实际拍摄时间
+            DateTime timeFalse = DateTime.ParseExact(photoFalseTime, "yyyy:MM:dd HH:mm:ss", culture); //该照片EXIF的记录时间
+            TimeSpan timeSpan = timeTrue - timeFalse; //计算照片时间差
+            FixExifDateTime(photoPath, timeSpan);
+        }
+
+        public static void FixExifDateTime(string photoPath, TimeSpan timeSpan)
+        {
+            var files = Directory.GetFiles(photoPath); //获取所有照片路径
+            IFormatProvider culture = new CultureInfo("zh-CN", true);
+            foreach (var file in files)
+            {
+                //替代System.Drawing.Image.FromFile(file);
+                System.Drawing.Image image = GetImage(file);
+
+                //获取Exif的DateTimeOriginal属性（36867）
+                PropertyItem pi = image.GetPropertyItem(36867); //(int)ExifTags.DateTimeOriginal
+
+                //转为时间文本
+                string oldTime = Encoding.ASCII.GetString(pi.Value);
+                oldTime = oldTime.Replace("\0", "");
+
+                Debug.WriteLine(file + " " + oldTime);
+                //时间文本格式化为DateTime
+                DateTime time = DateTime.ParseExact(oldTime, "yyyy:MM:dd HH:mm:ss", culture);
+
+                //由于是接着ExifToolGui没有改完的目录，所以只转换EXIF记录为timeFalse年份的，
+                //跨年的话要另外处理，因为我这里不跨年，所以简单点
+
+                //得到正确的时间
+                DateTime newTime = time + timeSpan;
+
+                //转换为EXIF存储的时间格式
+                string newTimeString = newTime.ToString("yyyy:MM:dd HH:mm:ss");
+                pi.Value = Encoding.ASCII.GetBytes(newTimeString + "\0");
+
+                //修改DateTimeOriginal属性和其他的时间属性
+                image.SetPropertyItem(pi);
+                pi.Id = 306; // (int)ExifTags.DateTime; //306
+                image.SetPropertyItem(pi);
+                pi.Id = 36868; //(int)ExifTags.DateTimeDigitized; //36868
+                image.SetPropertyItem(pi);
+
+                //存回文件
+                image.Save(file);
+
+                image.Dispose();
+            }
+        }
+
+        public static DateTime GetDateTimeOriginal(System.Drawing.Image image)
+        {
+            PropertyItem pi = image.GetPropertyItem(36867); //(int)ExifTags.DateTimeOriginal
+            string oldTime = Encoding.ASCII.GetString(pi.Value);
+            oldTime = oldTime.Replace("\0", "");
+
+            //时间文本格式化为DateTime
+            IFormatProvider culture = new CultureInfo("zh-CN", true);
+            DateTime time = DateTime.ParseExact(oldTime, "yyyy:MM:dd HH:mm:ss", culture);
+            return time;
+        }
+
+        public static System.Drawing.Image GetImage(string filePath)
+        {
+            try
+            {
+	            if (!File.Exists(filePath))
+	            {
+	                return null;
+	            }
+	
+	            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+	            {
+	                byte[] bytes = new byte[fileStream.Length];
+	                fileStream.Read(bytes, 0, bytes.Length);
+	
+	                MemoryStream memoryStream = new MemoryStream(bytes);
+	                if (memoryStream != null)
+	                {
+	                    return System.Drawing.Image.FromStream(memoryStream);
+	                }
+	            }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            return null;
+        }
+    }
+}
